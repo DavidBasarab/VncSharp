fe30f1078745b9464351d777f25bc1386f88996e
DavidBasarab <dbasarab617@gmail.com>
2015-11-16 15:29:35 -0500

    Added VNC Stream stub.

---
+++ VncSharp/VNCStream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VncSharp
+{
+    /// <summary>
+    /// This will be a simple VNC stream that will Give back an Image of the deskotp when a change is encoutered.  A simple wrapper.
+    /// </summary>
+    public class VNCStream
+    {
+    }
+}
